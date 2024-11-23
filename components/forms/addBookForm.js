29708cfa861c626833f86ba9a3cf133ca10ec34e
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2024-11-23 09:29:09 -0600

    fixed edit book not showing author dropdown bug

--- components/forms/addBookForm.js
+++ components/forms/addBookForm.js
@@ -3,7 +3,7 @@ import renderToDOM from '../../utils/renderToDom';
 import selectAuthor from './selectAuthor';
 
 // USING THIS FORM FOR BOTH CREATE AND UPDATE
-const addBookForm = (obj = {}, user) => {
+const addBookForm = (obj, user) => {
   clearDom();
   const domString = `
     <form id="${obj.firebaseKey ? `update-book--${obj.firebaseKey}` : 'submit-book'}" class="mb-4">
